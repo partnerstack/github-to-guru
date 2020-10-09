380618da4376a02adf13f32319e1f11ba6fd96c4
Althea Yi <althea.yi@partnerstack.com>
2020-10-09 10:34:41 -0400

    refactor/ cleans up update_markdown.js + main.yml action

--- update_markdown.js
+++ update_markdown.js
@@ -8,46 +8,24 @@ let cardConfigs = yaml.parse(
 );
 
 for (let cardFilename in cardConfigs) {
-    //TODO - add some conditional logic to only set unique tag value if no `tagValue`
-    let file = fs.readFileSync(path.resolve(`${cardFilename}`), "utf8")
-    let arr = file.split(/\r?\n/);
+    let markdownFile = fs.readFileSync(path.resolve(`${cardFilename}`), "utf8")
+    let arr = markdownFile.split(/\r?\n/);
     var existingTag
-    let existingUniqueTag = arr.forEach((line, idx) => {
+    arr.forEach((line, idx) => {
         if (line.includes("Guru tag - ")) {
             var line_arr = line.split(" ")
-            console.log("line arr", line_arr)
             existingTag = line_arr[line_arr.length - 1]
             return true
         } else {
             return false
         }
     });
-    console.log("existingUniqueTag", existingTag)
 
 
-
-
-    var uniqueTagValue
-    let content
     if (!existingTag) {
-        console.log("No existing unique tag value... generating")
-        uniqueTagValue = uuidv4()
+        let uniqueTagValue = uuidv4()
         let uniqueTagValueToWrite = `\nGuru tag - ${uniqueTagValue}`;
 
-        console.log("unique tag value to write", uniqueTagValueToWrite);
-
         fs.appendFileSync(path.resolve(`${cardFilename}`), uniqueTagValueToWrite, { flag: "as" })
-        file = fs.readFileSync(path.resolve(`${cardFilename}`), "utf8")
-        console.log('The "data to append" was appended to file!', file);
-        content = file
-
-    } else {
-        console.log(`Unique tag value found: ${uniqueTagValue}`)
-        uniqueTagValue = existingTag
-        content = file
     }
-
-    console.log("CONTENT", content)
 }
-
-
