0d951d1238b5be35ed31b5f93c8ce35a6976aee2
Althea Yi <althea.yi@partnerstack.com>
2020-10-09 11:28:53 -0400

    Cleans up comments

--- index.js
+++ index.js
@@ -71,37 +71,30 @@ async function apiSendStandardCard(
   arr.forEach((line, idx) => {
     if (line.includes("Guru tag - ")) {
       let line_arr = line.split(" ")
-      console.log("line arr", line_arr)
       existingTag = line_arr[line_arr.length - 1]
       return true
     } else {
       return false
     }
   });
-  console.log("existingUniqueTag", existingTag)
 
   let uniqueTagValue
   let content
   if (!existingTag) {
-    console.log("No existing unique tag value... generating")
+    console.log(`${cardFilename} has no existingTag. Generating... `)
     uniqueTagValue = uuidv4()
     let uniqueTagValueToWrite = `\nGuru tag - ${uniqueTagValue}`;
 
-    console.log("unique tag value to write", uniqueTagValueToWrite);
-
     fs.appendFileSync(path.resolve(`${cardFilename}`), uniqueTagValueToWrite, { flag: "as" })
     file = fs.readFileSync(path.resolve(`${cardFilename}`), "utf8")
-    console.log('The "data to append" was appended to file!', file);
+    console.log(`Appended a unique tag to ${cardFilename}: ${uniqueTagValueToWrite}`);
     content = file
 
   } else {
-    console.log(`Unique tag value found: ${existingTag}`)
+    console.log(`${cardFilename} has an existingTag: `, existingTag)
     uniqueTagValue = existingTag
     content = file
   }
-
-  console.log("CONTENT", content)
-
 
   if (process.env.GURU_CARD_YAML) {
     try {
