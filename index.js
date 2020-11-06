94c41a883d792d5d0a30a96a8ee7c41b3e15f3d7
Althea Yi <althea.yi@partnerstack.com>
2020-11-06 17:55:00 -0500

    attempt more error handling

--- index.js
+++ index.js
@@ -169,14 +169,30 @@ async function createCard(
 
   try {
     console.log("Making the card create request to Guru  now...")
-    return axios
-      .post(
-        `https://api.getguru.com/api/v1/facts/extended`,
-        cardData,
-        headers
-      )
+    return axios.post(
+      `https://api.getguru.com/api/v1/facts/extended`,
+      cardData,
+      headers
+    ).then((repsonse) => {
+      console.log("Response", response);
+    }).catch((error) => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+    })
   } catch (error) {
-    console.log(error.response.data)
     core.setFailed(
       `Unable to create card: ${error.message}`
     );
