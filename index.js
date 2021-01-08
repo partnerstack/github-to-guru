61034b72d351e2d62c2ba01597c5aa120a19734b
Althea Yi <althea.yi@partnerstack.com>
2021-01-08 11:00:17 -0500

    fix console clean

--- index.js
+++ index.js
@@ -474,10 +474,10 @@ async function apiSendStandardCard(
       line.includes(">") && line.includes("##") |
       line.includes("<") && line.includes("##")
     ) {
-      console.log("Special chars and H2s! We'll have to figure out how to handle this situation...")
+      console.log("Special chars + H2s; We'll have to figure out how to handle this situation...")
       return true
     } else if (line.includes("`") | line.includes(">") | line.includes("<")) {
-      console.log("Special chars! We'll have to figure out how to handle this situation...")
+      console.log("Special chars; We'll have to figure out how to handle this situation...")
       return true
     } else if (line.indexOf("**UUID H2 Guru Tag -** ") == 0) {
       console.log("This line is an existing H2 Tag...")
@@ -778,8 +778,8 @@ function getTagByValue(tags, tagValue) {
 }
 
 function getTagsInCategory(data, tagCategoryIndex) {
-  console.log("Gettings all Tags in the Category", tagsInCategory)
   let tagsInCategory = data[tagCategoryIndex].tags.map(tag => tag);
+  console.log("Got all Tags in Category", tagsInCategory)
   return tagsInCategory
 }
 
