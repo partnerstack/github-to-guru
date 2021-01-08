e11644db6ef34ae64fecb671ca5e0d1f99d70baa
Althea Yi <althea.yi@partnerstack.com>
2021-01-08 13:49:43 -0500

    wip refactor/ removes special char control flow from update_markdown.js

--- update_markdown.js
+++ update_markdown.js
@@ -24,16 +24,6 @@ for (let cardFilename in cardConfigs) {
             line_arr = line.split(" ")
             existingTag = line_arr[line_arr.length - 1]
             return true
-        } else if (
-            line.includes("`") && line.includes("##") |
-            line.includes(">") && line.includes("##") |
-            line.includes("<") && line.includes("##")
-        ) {
-            console.log("Special chars + H2s; We'll have to figure out how to handle this situation...")
-            return true
-        } else if (line.includes("`") | line.includes(">") | line.includes("<")) {
-            console.log("Special chars; We'll have to figure out how to handle this situation...")
-            return true
         } else if (line.indexOf("**UUID H2 Guru Tag -** ") == 0) {
             console.log("This line is an existing H2 Tag...")
             // add file line number to list
